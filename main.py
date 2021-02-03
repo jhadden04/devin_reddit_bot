e3a0b680d42368b61adcd9c906e90d8005990a33
John Hadden <59323260+jhadden04@users.noreply.github.com>
2021-02-03 19:09:58 +0000

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
 import praw
 from heapq import nlargest
-import requests
-import bs4
 from yahoo_fin import stock_info as si
+import time
 
 # can easily scrape the price using
 # https://finance.yahoo.com/quote/TSLA?p=TSLA&.tsrc=fin-srch. For this replace the TSLA with the correct ticker
@@ -24,7 +23,7 @@ def get_price(stock_ticker):
     finnhub_client = finnhub.Client(api_key="c0ddkhv48v6sgrj2de20")
     res = finnhub_client.stock_candles(stock_ticker, 'D', 1590988249, 1591852249)
     print(res)"""
-    return (si.get_live_price(stock_ticker))
+    return si.get_live_price(stock_ticker)
 
 
 numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0, " ", ".", ";", ")", ",", "$", "?", ">", "<", "\"", "!"]
@@ -33,56 +32,66 @@ all_stocks = []
 rankings = {}
 stock_prices = {}
 amount = {}
-for comment in reddit.subreddit('wallstreetbets').stream.comments():
-    if phrase in comment.body:
-        index = comment.body.index(phrase)
-        ticker = ""
-        ticker += comment.body[index]
-        x = 1
-        try:
 
-            if comment.body[index + x] in str(numbers):
-                continue
+
+def main():
+    for comment in reddit.subreddit('wallstreetbets').stream.comments():
+        try:
+            if phrase in comment.body:
+                index = comment.body.index(phrase)
+                ticker = ""
+                ticker += comment.body[index]
+                x = 1
+                try:
+
+                    if comment.body[index + x] in str(numbers):
+                        continue
+                except:
+                    continue
+                while True:
+                    try:
+
+                        if comment.body[index + x] not in numbers:
+                            ticker += comment.body[index + x]
+                            x += 1
+
+                        else:
+                            break
+                    except:
+                        break
+
+                ticker = ticker.upper()
+                if len(ticker) > 6:
+                    continue
+                # if ticker != "":
+                #   print(ticker)
+
+                if ticker not in all_stocks:
+                    all_stocks.append(ticker)
+                    rankings[ticker] = 1
+
+                elif ticker in all_stocks:
+                    rankings[ticker] += 1
+                # print(all_stocks)
+
+                largest = nlargest(5, rankings, key=rankings.get)
+
+                for i in range(len(largest) - 1):
+                    a = largest[i]
+                    no_dollar = a[1:]
+                    try:
+                        stock_prices[largest[i]] = get_price(no_dollar)
+                    except:
+                        continue
+                # print(rankings)
+                for i in range(len(largest) - 1):
+                    amount[largest[i]] = rankings[largest[i]]
+                print(amount)
+                print(largest)
+                print(stock_prices)
+                time.sleep(2)
         except:
             continue
-        while True:
-            try:
 
-                if comment.body[index + x] not in numbers:
-                    ticker += comment.body[index + x]
-                    x += 1
 
-                else:
-                    break
-            except:
-                break
-
-        ticker = ticker.upper()
-        if len(ticker) > 6:
-            continue
-        # if ticker != "":
-        #   print(ticker)
-
-        if ticker not in all_stocks:
-            all_stocks.append(ticker)
-            rankings[ticker] = 1
-
-        elif ticker in all_stocks:
-            rankings[ticker] += 1
-        # print(all_stocks)
-
-        largest = nlargest(5, rankings, key=rankings.get)
-
-        for i in range(len(largest) - 1):
-            a = largest[i]
-            no_dollar = a[1:]
-            try:
-                stock_prices[largest[i]] = get_price(no_dollar)
-            except:
-                continue
-        # print(rankings)
-        for i in range(len(largest)-1):
-            amount[largest[i]] = rankings[largest[i]]
-        print(amount)
-        print(largest)
-        print(stock_prices)
+main()
